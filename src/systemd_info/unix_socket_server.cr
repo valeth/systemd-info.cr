54766d821910479170819c8428439770c1675f49
Patrick Auernig <patrick.auernig@mykolab.com>
2018-06-15 01:06:38 +0200

    Add logger and quit on empty message

--- src/systemd_info/unix_socket_server.cr
+++ src/systemd_info/unix_socket_server.cr
@@ -1,27 +1,41 @@
 require "socket"
+require "./logger"
 require "./systemd_service"
 
 class UnixSocketServer
   property server : UNIXServer
 
   def initialize
+    LOG.info("Setting up socket...")
     @server = UNIXServer.new("/tmp/sysmon.sock")
-    at_exit { @server.close }
+    LOG.info("Socket ready.")
+    at_exit do
+      LOG.info("Shutting down...")
+      @server.close
+      LOG.info("bye")
+    end
   end
 
   def wait
     while client = @server.accept?
+      LOG.info("Client connected")
       spawn handle_request(client)
     end
   end
 
   private def handle_request(client : UNIXSocket)
-    puts "awaiting messages"
     loop do
+      LOG.debug("Awaiting message...")
       message = client.gets
+      return if message.nil? || message.empty?
+      LOG.debug("Got request for #{message}")
+      LOG.debug("Fetching info...")
       client.puts SystemdService.new(message).to_json unless message.nil?
+      LOG.debug("Done.")
     end
   rescue Errno
-    puts "connection closed"
+  ensure
+    client.close
+    LOG.error("Connection closed!")
   end
 end
